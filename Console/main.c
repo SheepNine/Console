3f6bd8835e842dbb2851a01c916e33a1b17d5e62
SheepNine <sheepnine@gmail.com>
2018-03-24 00:15:50 -0400

    Render some colours onto the screen

--- Console/main.c
+++ Console/main.c
@@ -14,6 +14,46 @@ int main(int argc, char** argv) {
 
 		if (window != NULL)
 		{
+			SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+			if (surface != NULL)
+			{
+				if (SDL_LockSurface(surface) == 0)
+				{
+					if (surface->format->format == SDL_PIXELFORMAT_RGB888)
+					{
+						int offset;
+						for (int y = 0; y < surface->h; y++)
+						{
+							for (int x = 0; x < surface->w; x++)
+							{
+								offset = (x + y * surface->w) * surface->format->BytesPerPixel;
+								((Uint8*)surface->pixels)[offset + 0] = 128; // Blue
+								((Uint8*)surface->pixels)[offset + 1] = 128; // Green
+								((Uint8*)surface->pixels)[offset + 2] = 0; // Red
+							}
+						}
+
+						offset = (0 + 0 * surface->w) * surface->format->BytesPerPixel;
+						((Uint8*)surface->pixels)[offset + 0] = 255; // Blue
+						((Uint8*)surface->pixels)[offset + 1] = 255; // Green
+						((Uint8*)surface->pixels)[offset + 2] = 255; // Red
+
+						offset = (surface->w - 1 + 0 * surface->w) * surface->format->BytesPerPixel;
+						((Uint8*)surface->pixels)[offset + 0] = 0; // Blue
+						((Uint8*)surface->pixels)[offset + 1] = 0; // Green
+						((Uint8*)surface->pixels)[offset + 2] = 255; // Red
+
+						offset = (0 + (surface->h - 1) * surface->w) * surface->format->BytesPerPixel;
+						((Uint8*)surface->pixels)[offset + 0] = 255; // Blue
+						((Uint8*)surface->pixels)[offset + 1] = 0; // Green
+						((Uint8*)surface->pixels)[offset + 2] = 0; // Red
+					}
+					SDL_UnlockSurface(surface);
+				}
+				SDL_UpdateWindowSurface(window);
+			}
+
 			SDL_bool done = SDL_FALSE;
 			while (!done)
 			{
