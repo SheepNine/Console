4f6922f888c4510f3ce261f6825994aceee54af6
SheepNine <sheepnine@gmail.com>
2018-03-24 13:09:25 -0400

    Render content from a content buffer

--- Console/main.c
+++ Console/main.c
@@ -1,6 +1,29 @@
 #include "SDL.h"
 #define SDL_WINDOW_NONE 0
 
+#define CONTENT_SIZE 248
+
+Uint8 R[CONTENT_SIZE * CONTENT_SIZE];
+Uint8 G[CONTENT_SIZE * CONTENT_SIZE];
+Uint8 B[CONTENT_SIZE * CONTENT_SIZE];
+
+void InitBuffer() {
+	for (int y = 0; y < CONTENT_SIZE; y++)
+	{
+		for (int x = 0; x < CONTENT_SIZE; x++)
+		{
+			R[x + CONTENT_SIZE * y] = x | y;
+			G[x + CONTENT_SIZE * y] = x & y;
+			B[x + CONTENT_SIZE * y] = x ^ y;
+		}
+	}
+}
+
+Uint8 Sample(Uint8* buffer, int x, int y, int scale) {
+	int srcOffset = (x / scale) + (y / scale) * CONTENT_SIZE;
+	return buffer[srcOffset];
+}
+
 void Draw(SDL_Window* window) {
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
 
@@ -12,42 +35,20 @@ void Draw(SDL_Window* window) {
 			{
 				SDL_memset4(surface->pixels, 0x00AAAAAA, surface->w * surface->h);
 
-				int scale;
-				if (surface->w > surface->h) {
-					scale = surface->h / 248;
-				} else {
-					scale = surface->w / 248;
-				}
-				int pictureOffsetX = (surface->w - 248 * scale) / 2;
-				int pictureOffsetY = (surface->h - 248 * scale) / 2;
-				int pictureOffset = (pictureOffsetX + surface->w * pictureOffsetY) * surface->format->BytesPerPixel;
+				int scale = SDL_min(surface->w, surface->h) / CONTENT_SIZE;
+				int contentOffsetX = (surface->w - CONTENT_SIZE * scale) / 2;
+				int contentOffsetY = (surface->h - CONTENT_SIZE * scale) / 2;
+				int contentOffset = (contentOffsetX + surface->w * contentOffsetY) * surface->format->BytesPerPixel;
 
-				int offset;
-				for (int y = 0; y < 248 * scale; y++)
-				{
-					for (int x = 0; x < 248 * scale; x++)
-					{
-						offset = pictureOffset + (x + y * surface->w) * surface->format->BytesPerPixel;
-						((Uint8*)surface->pixels)[offset + 0] = 128; // Blue
-						((Uint8*)surface->pixels)[offset + 1] = 128; // Green
-						((Uint8*)surface->pixels)[offset + 2] = 0; // Red
+				for (int y = 0; y < CONTENT_SIZE * scale; y++) {
+					for (int x = 0; x < CONTENT_SIZE * scale; x++) {
+						int destOffset = contentOffset + (x + surface->w * y) * surface->format->BytesPerPixel;
+
+						((Uint8*)surface->pixels)[destOffset + 0] = Sample(B, x, y, scale);
+						((Uint8*)surface->pixels)[destOffset + 1] = Sample(G, x, y, scale);
+						((Uint8*)surface->pixels)[destOffset + 2] = Sample(R, x, y, scale);
 					}
 				}
-
-				offset = (0 + 0 * surface->w) * surface->format->BytesPerPixel;
-				((Uint8*)surface->pixels)[offset + 0] = 255; // Blue
-				((Uint8*)surface->pixels)[offset + 1] = 255; // Green
-				((Uint8*)surface->pixels)[offset + 2] = 255; // Red
-
-				offset = (surface->w - 1 + 0 * surface->w) * surface->format->BytesPerPixel;
-				((Uint8*)surface->pixels)[offset + 0] = 0; // Blue
-				((Uint8*)surface->pixels)[offset + 1] = 0; // Green
-				((Uint8*)surface->pixels)[offset + 2] = 255; // Red
-
-				offset = (0 + (surface->h - 1) * surface->w) * surface->format->BytesPerPixel;
-				((Uint8*)surface->pixels)[offset + 0] = 255; // Blue
-				((Uint8*)surface->pixels)[offset + 1] = 0; // Green
-				((Uint8*)surface->pixels)[offset + 2] = 0; // Red
 			}
 			SDL_UnlockSurface(surface);
 		}
@@ -60,6 +61,8 @@ int main(int argc, char** argv) {
 
 	if (initResult == 0)
 	{
+		InitBuffer();
+
 		SDL_Window* window = SDL_CreateWindow(
 			"Console",
 			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -68,7 +71,7 @@ int main(int argc, char** argv) {
 
 		if (window != NULL)
 		{
-			SDL_SetWindowMinimumSize(window, 248, 248);
+			SDL_SetWindowMinimumSize(window, CONTENT_SIZE, CONTENT_SIZE);
 			Draw(window);
 
 			SDL_bool done = SDL_FALSE;
