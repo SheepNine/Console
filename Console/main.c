aec03f7e68d902f492556b546a208c1129460ff6
SheepNine <sheepnine@gmail.com>
2018-03-25 11:18:55 -0400

    Wrap screen buffer in a struct

--- Console/main.c
+++ Console/main.c
@@ -3,50 +3,70 @@
 
 #define CONTENT_SIZE 248
 
-Uint8 R[CONTENT_SIZE * CONTENT_SIZE];
-Uint8 G[CONTENT_SIZE * CONTENT_SIZE];
-Uint8 B[CONTENT_SIZE * CONTENT_SIZE];
+typedef struct screen_def screen, *h_screen;
 
-void SetPixel(Uint8 x, Uint8 y, Uint8 r, Uint8 g, Uint8 b, SDL_bool translucent) {
+struct screen_def {
+	Uint8 R[CONTENT_SIZE * CONTENT_SIZE];
+	Uint8 G[CONTENT_SIZE * CONTENT_SIZE];
+	Uint8 B[CONTENT_SIZE * CONTENT_SIZE];
+};
+
+void clear_screen(h_screen screen, Uint8 r, Uint8 g, Uint8 b) {
+	SDL_memset(screen->R, r, CONTENT_SIZE * CONTENT_SIZE);
+	SDL_memset(screen->G, g, CONTENT_SIZE * CONTENT_SIZE);
+	SDL_memset(screen->B, b, CONTENT_SIZE * CONTENT_SIZE);
+}
+
+h_screen create_screen() {
+	h_screen result = (h_screen)SDL_malloc(sizeof(screen));
+	clear_screen(result, 0, 255, 255);
+	return result;
+}
+
+void destroy_screen(h_screen screen) {
+	SDL_free(screen);
+}
+
+void SetPixel(h_screen screen, Uint8 x, Uint8 y, Uint8 r, Uint8 g, Uint8 b, SDL_bool translucent) {
 	SDL_assert(x < CONTENT_SIZE);
 	SDL_assert(x < CONTENT_SIZE);
 
 	int offset = x + y * CONTENT_SIZE;
 	if (translucent) {
-		R[offset] = (R[offset] + r) >> 1;
-		G[offset] = (G[offset] + g) >> 1;
-		B[offset] = (B[offset] + b) >> 1;
+		screen->R[offset] = (screen->R[offset] + r) >> 1;
+		screen->G[offset] = (screen->G[offset] + g) >> 1;
+		screen->B[offset] = (screen->B[offset] + b) >> 1;
 	} else {
-		R[offset] = r;
-		G[offset] = g;
-		B[offset] = b;
+		screen->R[offset] = r;
+		screen->G[offset] = g;
+		screen->B[offset] = b;
 	}
 }
 
-void SetPixelPacked(Uint8 x, Uint8 y, Uint16 color) {
-	SetPixel(x, y,
+void SetPixelPacked(h_screen screen, Uint8 x, Uint8 y, Uint16 color) {
+	SetPixel(screen, x, y,
 		((color & 0x7C00) >> 7), 
 		((color & 0x03E0) >> 2), 
 		((color & 0x001F) << 3), 
 		(color & 0x8000 ? SDL_TRUE : SDL_FALSE));
 }
 
-void SetPixelRoundTrip(Uint8 x, Uint8 y, Uint8 r, Uint8 g, Uint8 b, SDL_bool translucent) {
-	SetPixelPacked(x, y,
+void SetPixelRoundTrip(h_screen screen, Uint8 x, Uint8 y, Uint8 r, Uint8 g, Uint8 b, SDL_bool translucent) {
+	SetPixelPacked(screen, x, y,
 		(translucent ? 0x8000 : 0x0000) |
 		((r & 0xF8) << 7) |
 		((g & 0xF8) << 2) |
 		((b & 0xF8) >> 3));
 }
 
-void InitBuffer() {
+void InitBuffer(h_screen screen) {
 	for (int y = 0; y < CONTENT_SIZE; y++)
 	{
 		for (int x = 0; x < CONTENT_SIZE; x++)
 		{
-			SetPixelRoundTrip(x, y, x | y, x & y, x ^ y, SDL_FALSE);
+			SetPixelRoundTrip(screen, x, y, x | y, x & y, x ^ y, SDL_FALSE);
 		}
-		SetPixelRoundTrip(y / 2, y, 255, 255, 255, SDL_TRUE);
+		SetPixelRoundTrip(screen, y / 2, y, 255, 255, 255, SDL_TRUE);
 	}
 
 }
@@ -56,7 +76,7 @@ Uint8 Sample(Uint8* buffer, int x, int y, int scale) {
 	return buffer[srcOffset];
 }
 
-void Draw(SDL_Window* window) {
+void Draw(h_screen screen, SDL_Window* window) {
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
 
 	if (surface != NULL)
@@ -76,9 +96,9 @@ void Draw(SDL_Window* window) {
 					for (int x = 0; x < CONTENT_SIZE * scale; x++) {
 						int destOffset = contentOffset + (x + surface->w * y) * surface->format->BytesPerPixel;
 
-						((Uint8*)surface->pixels)[destOffset + 0] = Sample(B, x, y, scale);
-						((Uint8*)surface->pixels)[destOffset + 1] = Sample(G, x, y, scale);
-						((Uint8*)surface->pixels)[destOffset + 2] = Sample(R, x, y, scale);
+						((Uint8*)surface->pixels)[destOffset + 0] = Sample(screen->B, x, y, scale);
+						((Uint8*)surface->pixels)[destOffset + 1] = Sample(screen->G, x, y, scale);
+						((Uint8*)surface->pixels)[destOffset + 2] = Sample(screen->R, x, y, scale);
 					}
 				}
 			}
@@ -93,7 +113,8 @@ int main(int argc, char** argv) {
 
 	if (initResult == 0)
 	{
-		InitBuffer();
+		h_screen screen = create_screen();
+		InitBuffer(screen);
 
 		SDL_Window* window = SDL_CreateWindow(
 			"Console",
@@ -104,7 +125,7 @@ int main(int argc, char** argv) {
 		if (window != NULL)
 		{
 			SDL_SetWindowMinimumSize(window, CONTENT_SIZE, CONTENT_SIZE);
-			Draw(window);
+			Draw(screen, window);
 
 			SDL_bool done = SDL_FALSE;
 			while (!done)
@@ -118,10 +139,10 @@ int main(int argc, char** argv) {
 							done = SDL_TRUE;
 						}
 						if (evt.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
-							Draw(window);
+							Draw(screen, window);
 						}
 						if (evt.window.event == SDL_WINDOWEVENT_EXPOSED) {
-							Draw(window);
+							Draw(screen, window);
 						}
 						break;
 					}
@@ -132,6 +153,8 @@ int main(int argc, char** argv) {
 
 			SDL_DestroyWindow(window);
 		}
+
+		destroy_screen(screen);
 	}
 
 	SDL_Quit();
